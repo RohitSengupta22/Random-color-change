020aa8836f3a100403f7b05114f99858e1dd0e61
RohitSengupta22 <chints.rsg@gmail.com>
2023-09-26 10:39:16 +0530

    minor change

--- script.js
+++ script.js
@@ -5,20 +5,20 @@ async function changeColor(){
 
    await fetch(url)
   .then(response => {
-    // Check if the response status is OK (status code 200)
+    
     if (!response.ok) {
       throw new Error(`Network response was not ok (status ${response.status})`);
     }
-    // Parse the response body as JSON
+    
     return response.json();
   })
   .then(data => {
-    // Handle the JSON data here
+   
     console.log(data);
     document.getElementById("color").style.backgroundColor = data.hex;
   })
   .catch(error => {
-    // Handle errors here
+    
     console.error('Fetch error:', error);
   });
 
